e2ae7c4475bdf296dead5bc0490924aaaedab33d
piupop <mrmr.03780710@gmail.com>
2017-05-18 23:48:36 +0900

    0518update

--- app/src/main/java/com/lifeistech/android/eventreminder/AddeventActivity.java
+++ app/src/main/java/com/lifeistech/android/eventreminder/AddeventActivity.java
@@ -18,13 +18,21 @@ import io.realm.Sort;
 
 public class AddeventActivity extends AppCompatActivity {
     Realm realm;
-    Intent intent;
+
     Button addevent;
     EditText editDate1;
     EditText editDate2;
     EditText editTitle;
     EditText editRateNum;
     EditText editMemo;
+    String reEditDate1;
+    String reEditDate2;
+    String reEditTitle;
+    String reEditRate;
+    String reEditMemo;
+    int reEditId;
+    Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,7 @@ public class AddeventActivity extends AppCompatActivity {
         Realm.init(this);
         realm = Realm.getDefaultInstance();
 
+
         intent = new Intent();
         addevent = (Button)findViewById(R.id.addevent);
         editDate1 = (EditText) findViewById(R.id.date1);
@@ -43,63 +52,87 @@ public class AddeventActivity extends AppCompatActivity {
         editRateNum = (EditText) findViewById(R.id.rateNum);
         editMemo = (EditText) findViewById(R.id.memo);
 
-        //intentの受け取りとテキストの表示
         Intent intent = getIntent();
-        String reEditDate1 = intent.getStringExtra("date1ToEdit");
-        String reEditDate2 = intent.getStringExtra("date2ToEdit");
-        String reEditTitle = intent.getStringExtra("titleToEdit");
-        String reEditMemo = intent.getStringExtra("memoToEdit");
-        String reEditRate = intent.getStringExtra("rateToEdit");
-        int reEditId = intent.getIntExtra("idToEdit", 0);
+        reEditDate1 = intent.getStringExtra("date1ToEdit");
+        reEditDate2 = intent.getStringExtra("date2ToEdit");
+        reEditTitle = intent.getStringExtra("titleToEdit");
+        reEditMemo = intent.getStringExtra("memoToEdit");
+        reEditRate = intent.getStringExtra("rateToEdit");
+        reEditId = intent.getIntExtra("idToEdit", 0);
 
+        //intentで受け取ったテキストを表示
         editDate1.setText(reEditDate1);
         editDate2.setText(reEditDate2);
         editTitle.setText(reEditTitle);
         editMemo.setText(reEditMemo);
         editRateNum.setText(reEditRate);
-
     }
 
     public void add(View v){
+        
+        if ( ) {
+            realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm bgRealm) {
 
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm bgRealm) {
+                    RealmResults<MyModel> result = bgRealm.getDefaultInstance().where(MyModel.class).findAll();
+                    int id = 0;
+                    if (result.size() != 0) {
+                        id = result.sort("id", Sort.ASCENDING).last().getId() + 1;
+                    } //realmにデータが存在するときはIDを追加
 
-                RealmResults<MyModel> result = bgRealm.getDefaultInstance().where(MyModel.class) .findAll();
-                int id =0;
-                if (result.size() != 0 ){
-                    id = result.sort("id", Sort.ASCENDING).last().getId()+1;
-                } //realmにデータが存在するときはIDを追加
+                    MyModel model = bgRealm.createObject(MyModel.class, id);
+                    String date1Text = editDate1.getText().toString();
+                    String date2Text = editDate2.getText().toString();
+                    String titleText = editTitle.getText().toString();
+                    String rateNumText = editRateNum.getText().toString();
+                    int rateNum = Integer.parseInt(rateNumText);//int型への変換
+                    String memoText = editMemo.getText().toString();
 
-                MyModel model = bgRealm.createObject(MyModel.class,id);
-                String date1Text = editDate1.getText().toString();
-                String date2Text = editDate2.getText().toString();
-                String titleText = editTitle.getText().toString();
-                String rateNumText = editRateNum.getText().toString();
-                int rateNum = Integer.parseInt(rateNumText);//int型への変換
-                String memoText = editMemo.getText().toString();
+                    model.setDate1(date1Text);
+                    model.setDate2(date2Text);
+                    model.setTitle(titleText);
+                    model.setRate(rateNum);
+                    model.setMemo(memoText);
+                }
 
-                model.setDate1(date1Text);
-                model.setDate2(date2Text);
-                model.setTitle(titleText);
-                model.setRate(rateNum);
-                model.setMemo(memoText);}
+            }, new Realm.Transaction.OnSuccess() {
+                @Override
+                public void onSuccess() {
+                    // Transaction was a success.
+                    finish();
+                }
+            }, new Realm.Transaction.OnError() {
+                @Override
+                public void onError(Throwable error) {
+                    // Transaction failed and was automatically canceled.
+                    Log.e("ErrorTransaction", error.toString());
+                }
+            });
+        } else {
+            realm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm bgRealm) {
+                    RealmResults<MyModel> result2 = bgRealm.getDefaultInstance().where(MyModel.class).equalTo("id", reEditId).findAll();
+                    String date1Text = editDate1.getText().toString();
+                    String date2Text = editDate2.getText().toString();
+                    String titleText = editTitle.getText().toString();
+                    Log.e("editTitle", titleText);
+                    String rateNumText = editRateNum.getText().toString();
+                    int rateNum = Integer.parseInt(rateNumText);//int型への変換
+                    String memoText = editMemo.getText().toString();
 
-        }, new Realm.Transaction.OnSuccess() {
-            @Override
-            public void onSuccess() {
-                // Transaction was a success.
-                finish();
-            }
-        }, new Realm.Transaction.OnError() {
-            @Override
-            public void onError(Throwable error) {
-                // Transaction failed and was automatically canceled.
-                Log.e("ErrorTransaction",error.toString());
-            }
-        });
-    }
+                    MyModel edit = bgRealm.createObject(MyModel.class);
+                    edit.setDate1(date1Text);
+                    edit.setDate2(date2Text);
+                    edit.setTitle(titleText);
+                    edit.setRate(rateNum);
+                    edit.setMemo(memoText);
+
+                }
+            });
+        }
+    };
 
     public void cancel(View v){
         finish();
